6c633ead90610f0994efcd020cb7abe649869890
SNIPRS <taozhang1917@gmail.com>
2020-07-24 12:00:28 -0700

    updated models and analysis

--- runs/analysis.py
+++ runs/analysis.py
@@ -1,51 +1,53 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
 class Analysis:
-    def __init__(self):
-        self.crop = 100
-        self.fsize = 6
-        self.histogram_bins = 40
+    crop = 100
+    fsize = 6
+    histogram_bins = 40
         
-    def wrap_phi(self, var):
+    def wrap_phi(var):
         var = var%(2*np.pi)
         var = var - 2*np.pi*(var > np.pi)
         return var
         
-    def predictions_vs_sample(self, compare, true, names, wrap_phi):
-        plt.figure(figsize=(self.fsize*2, self.fsize*len(names)))
+    def predictions_vs_sample(compare, true, names, wrap_phi):
+        plt.figure(figsize=(Analysis.fsize*2, Analysis.fsize*len(names)))
         for i in range(0, len(names)):
-            compare_small = compare[:self.crop,i]
-            true_small = true[:self.crop,i]
+            compare_small = compare[:Analysis.crop,i]
+            true_small = true[:Analysis.crop,i]
             if wrap_phi and "phi" in names[i]:
-                compare_small = self.wrap_phi(compare_small)
-                true_small = self.wrap_phi(true_small)
+                compare_small = Analysis.wrap_phi(compare_small)
+                true_small = Analysis.wrap_phi(true_small)
             plt.subplot(len(names), 1, i+1)
-            plt.plot(range(0,self.crop), compare_small, 'bo', markersize=3, label = 'Predictions')
-            plt.plot(range(0,self.crop), true_small, 'ro', markersize=3, label = 'True Value')
+            plt.plot(range(0,Analysis.crop), compare_small, 'bo', markersize=3, label = 'Predictions')
+            plt.plot(range(0,Analysis.crop), true_small, 'ro', markersize=3, label = 'True Value')
             ym, yM = plt.ylim()
-            for x in range(self.crop):
+            for x in range(Analysis.crop):
                 plt.vlines(x, color='g', linestyle='-', alpha=0.2, ymin= 
                             min(compare_small[x], true_small[x]), 
                             ymax= max(compare_small[x], true_small[x]))
-            plt.hlines(np.mean(true[:,i]), xmin=-20, xmax=self.crop+20, alpha=0.5)
+            plt.hlines(np.mean(true[:,i]), xmin=-20, xmax=Analysis.crop+20, alpha=0.5)
             MSE = 1/compare[:,i].size*np.sum((compare[:,i]- true[:,i])**2)
             plt.xlabel('Sample')
-            plt.xlim(0, self.crop)
+            plt.xlim(0, Analysis.crop)
             plt.ylabel(names[i])
             plt.title(names[i] + " MSE: " + str(MSE))
             plt.legend()
     
-    def display_errors(self, compare, true, names, wrap_phi):
+    def display_errors(compare, true, names, wrap_phi):
         MSE = 1/compare.size*np.sum((compare- true)**2)
         print("total MSE: " + str(MSE))
         print(" ")
         for i in range(len(names)):
             diff = compare[:,i] -true[:,i]
             if wrap_phi and "phi" in names[i]:
-                diff = self.wrap_phi(diff)
+                diff = Analysis.wrap_phi(diff)
             MSE = 1/compare[:,i].size*np.sum((diff)**2)
             print("{0} MSE : ".format(names[i]), '%.10f'%MSE)
     
-    def difference_histogram(self, compare, true, names, wrap_phi, bins):
-        plt.figure(figsize=(self.fsize*2,self.fsize*len(names)))
+    def difference_histogram(compare, true, names, wrap_phi, bins):
+        plt.figure(figsize=(Analysis.fsize*2,Analysis.fsize*len(names)))
         for i in range(len(names)):
             plt.subplot(len(names), 1, i+1)
             diff = true[:,i] - compare[:,i]
@@ -60,15 +62,15 @@ class Analysis:
             plt.legend()
             plt.ylabel('Frequency')
             
-    def variable_histogram(self, compare, true, names, wrap_phi, bins): 
-        plt.figure(figsize=(self.fsize*2,self.fsize*len(names)))
+    def variable_histogram(compare, true, names, wrap_phi, bins): 
+        plt.figure(figsize=(Analysis.fsize*2,Analysis.fsize*len(names)))
         for i in range(len(names)):
             plt.subplot(len(names), 1, i+1)
             compare_small = compare[:, i]
             true_small = true[:, i]
             if wrap_phi and "phi" in names[i]:
-                compare_small = self.wrap_phi(compare_small)
-                true_small = self.wrap_phi(true_small)
+                compare_small = Analysis.wrap_phi(compare_small)
+                true_small = Analysis.wrap_phi(true_small)
             hist0, bin_edges = np.histogram(true[:, i], bins=40)
             
             if bins[i] is None:
@@ -83,8 +85,8 @@ class Analysis:
             plt.legend()
             plt.ylabel('Frequency')
     
-    def difference_vs_variable(self, compare, true, names, wrap_phi):
-        plt.figure(figsize=(self.fsize*2,self.fsize*len(names)))
+    def difference_vs_variable(compare, true, names, wrap_phi):
+        plt.figure(figsize=(Analysis.fsize*2,Analysis.fsize*len(names)))
         for i in range(len(names)):
             plt.subplot(len(names), 1, i+1)
             plt.plot(true[:, i], true[:, i]-compare[:, i], 'o', color='purple', label='True - Predicted', markersize=2)
@@ -92,8 +94,8 @@ class Analysis:
             plt.legend()
             plt.ylabel('Difference')
     
-    def predicted_vs_true(self, compare, true, names, wrap_phi):
-        plt.figure(figsize=(self.fsize*2,self.fsize*len(names)))
+    def predicted_vs_true(compare, true, names, wrap_phi):
+        plt.figure(figsize=(Analysis.fsize*2,Analysis.fsize*len(names)))
         for i in range(len(names)):
             plt.subplot(len(names), 1, i+1)
             plt.plot(true[:, i], compare[:, i], 'o', color='g', markersize=2)
@@ -102,3 +104,5 @@ class Analysis:
             plt.xlabel('True')
             plt.title(names[i])
             plt.ylabel('Predicted')
+            
+            
--- runs/models.py
+++ runs/models.py
@@ -27,3 +27,39 @@ def double_LSTM_hyper1():
     
     return model 
 
+
+def jet_weights():
+    jet_input = Input(shape=(trainX_jets.shape[1], trainX_jets.shape[2]))
+    Mask = Masking(-2)(jet_input)
+    other_input = Input(shape=(trainX_other.shape[1]))
+    flat_jets =  Flatten()(jet_input)
+    concat0 = concatenate([other_input, flat_jets])
+    PreDense1 = Dense(128, activation='tanh')(concat0)
+    PreDense2 = Dense(64, activation='tanh')(PreDense1)
+    PreDense3 = Dense(trainX_jets.shape[1], activation='softmax')(PreDense2)
+    Norm = Lambda(lambda x: tf.math.multiply(x, 4))(PreDense3) # 4 relevant jets
+    Shape_Dot = Reshape((-1,1))(Norm)
+    Dot_jets = Multiply()([Shape_Dot, Mask])
+    
+    TDDense11 = TimeDistributed(Dense(64, activation='relu'))(Dot_jets)
+    TDDense12 = TimeDistributed(Dense(64, activation='relu'))(TDDense11)
+    Sum = Flatten()(TDDense12)
+    # Sum = Lambda(lambda x: tf.reduce_sum(x,1))(TDDense12)
+    Dense13 = Dense(64, activation='relu')(Sum)
+    flat_right = Flatten()(Dense13)
+    
+    
+    Dense21 = Dense(64, activation='relu')(other_input)
+    Dense22 = Dense(64, activation='relu')(Dense21)
+    flat_other = Flatten()(Dense22)
+    
+    concat = concatenate([flat_other, flat_right])
+    dense1 = Dense(256, activation='relu')(concat)
+    dense2 = Dense(128, activation='relu')(dense1)
+    output = Dense(len(Y_names), activation='linear')(dense2)
+    
+    model = keras.models.Model(inputs=[jet_input, other_input], outputs=output)
+    optimizer = keras.optimizers.Adam(learning_rate=5e-5)
+    model.compile(loss='mse', optimizer= optimizer, metrics=['mse'])
+    
+    return model 
